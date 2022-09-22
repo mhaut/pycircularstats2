bec3d9a6a1e2900967dd9aff4f838dd61e7d18d8
mhaut <mhaut@users.noreply.github.com>
2022-09-22 19:34:42 +0200

    fix typos

--- pycircularstats/fileIO.py
+++ pycircularstats/fileIO.py
@@ -18,42 +18,48 @@ def correct_type(typedata, data):
     elif typedata == 'vectors' and data.shape[1] == 4: return True
     else: return False # not correct
 
+def data2res(typedata, data):
+    res = np.zeros((data.shape[0], 9))
+    if typedata == 'cartesian':
+        res[:, 4:8] = np.array([data[:, i] for i in range(4)]).T
+        rectangular_vectors = pyCconvert.tocalculateincr(data)
+        polar_vectors       = pyCconvert.vectors2polar(rectangular_vectors)
+    elif typedata == 'incremental':
+        rectangular_vectors = data
+        polar_vectors       = pyCconvert.vectors2polar(data)
+    elif typedata == 'polar':
+        rectangular_vectors = pyCconvert.vectors2rectangular(data)
+        polar_vectors       = data
+    elif typedata == 'vectors':
+        res[:, :4] = np.array([data[:, i] for i in range(4)]).T
+        return res
+    res[:,:2]  = polar_vectors
+    res[:,2:4] = rectangular_vectors
+    return res
 
 def loaddata(path, typedata = 'incremental'):
     if not path:
         print('error, not path file')
-        exit()
+        #exit()
+        return None
     data = readfromfile(path)
     if data.shape[0] > 1:
         TYPES_AVAIL = ['cartesian', 'incremental', 'polar', 'vectors']
         if typedata.lower() not in TYPES_AVAIL:
             print("Error, format selected is not avaiable")
             print(TYPES_AVAIL)
-            exit()
+            return None
+            #exit()
         elif correct_type(typedata, data) == False:
             print("Error, the file is not in the correct format.")
-            exit()
+            return None
+            #exit()
         else:
-            res = np.zeros((data.shape[0], 9))
-            if typedata == 'cartesian':
-                res[:, 4:8] = np.array([data[:, i] for i in range(4)]).T
-                rectangular_vectors = pyCconvert.tocalculateincr(data)
-                polar_vectors       = pyCconvert.vectors2polar(rectangular_vectors)
-            elif typedata == 'incremental':
-                rectangular_vectors = data
-                polar_vectors       = pyCconvert.vectors2polar(data)
-            elif typedata == 'polar':
-                rectangular_vectors = pyCconvert.vectors2rectangular(data)
-                polar_vectors       = data
-            elif typedata == 'vectors':
-                res[:, :4] = np.array([data[:, i] for i in range(4)]).T
-                return res
-            res[:,:2]  = polar_vectors
-            res[:,2:4] = rectangular_vectors
+            return data2res(typedata, data)
     else:
         print("Error, in read file, check it!")
-        exit() 
-    return res
+        #exit() 
+        return None
 
 
 def text2file(text, pathname):

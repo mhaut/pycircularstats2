2bcd12f653eed6535e1aea96fd445ab2441ff191
mhaut <mhaut@users.noreply.github.com>
2022-09-28 23:30:32 +0200

    fix typos on convert

--- pycircularstats/convert.py
+++ pycircularstats/convert.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-def vectors2rectangular(vectors, init=0):
+def vectors2rectangular(vectors, init=1):
     rectangular_vectors = np.zeros((vectors.shape))
     module  = vectors[:, 0]
     grades  = vectors[:, 1]
@@ -20,24 +20,35 @@ def vectors2rectangular(vectors, init=0):
     #rectangular_vectors.append(np.sin(np.radians(grades)) * module)
     #return rectangular_vectors
 
-def vectors2rectangularMAP(module, grades):
+def vectors2rectangularMAP(module, grades, init=1):
     rectangular_vectors = np.ones((len(modules), 2))
-    rectangular_vectors[:, 0] = np.sin(np.radians(grades)) * module
-    rectangular_vectors[:, 1] = np.cos(np.radians(grades)) * module
+    val1 = np.degrees(np.cos(np.radians(grades))) * module # x
+    val2 = np.degrees(np.sin(np.radians(grades))) * module # y
+    rectangular_vectors[:, 0] = val1 if init == 0 else val2
+    rectangular_vectors[:, 1] = val2 if init == 0 else val1
     return rectangular_vectors
 
+#def vectors2polar(vectors):
+    #num_data = vectors.shape[0]
+    #x = vectors[:,0]
+    #y = vectors[:,1]
+    #module = np.sqrt(x**2 + y**2)
+    ##grades = np.degrees(np.arctan2(y,x))
+    #grades = np.degrees(np.arctan2(y,x))
+    ##grades = np.degrees(np.arctan2(x,y))
+    ##grades = np.degrees(np.arctan(x/y))
+    #grades[grades<0] += 360
+    #polar_vectors = np.array([module, grades]).T
+    #return polar_vectors
+
 def vectors2polar(vectors):
-    num_data = vectors.shape[0]
     x = vectors[:,0]
     y = vectors[:,1]
     module = np.sqrt(x**2 + y**2)
-    #grades = np.degrees(np.arctan2(y,x))
     grades = np.degrees(np.arctan2(x,y))
-    #grades = np.degrees(np.arctan(x/y))
     grades[grades<0] += 360
     polar_vectors = np.array([module, grades]).T
     return polar_vectors
-
 
 def getpolarvalues(scale, data_x, data_y):
     [module, theta] = vectors2polar(np.stack((data_x, data_y), axis=1)).T

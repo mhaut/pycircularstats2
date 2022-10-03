3f7e18cd192b66850fde3c36339bf7a0ae7e57b7
mhaut <mhaut@users.noreply.github.com>
2022-10-03 17:13:21 +0200

    improvements

--- examples/example.py
+++ examples/example.py
@@ -7,37 +7,34 @@ import numpy as np
 
 
 def main():
-    path = '../datasets/MeasuredRealData.txt'
-    data = pyCfileIO.loaddata(path, typedata='cartesian')
-    path = '../datasets/RectangularData.txt'
-    data = pyCfileIO.loaddata(path, typedata='incremental')
+    for fname, typeD in zip(['MeasuredRealData','RectangularData'],['cartesian', 'incremental']):
+        path = '../datasets/'+fname+'.txt'
+        data = pyCfileIO.loaddata(path, typedata=typeD)
 
-    modules = data[:,0]
-    azimuths = data[:,1]
-    values = pyCmath.allmodulestatistics(data[:,0], ndig = 4)
-    print(values)
-    values = pyCmath.allazimuthstatistic(data[:,1], ndig = 4)
-    print(values)
-    values = pyCmath.raotest(azimuths)
-    print(values)
-    values = pyCmath.rayleightest(azimuths)
-    print(values)
-    X_coordinate = data[:,2]
-    Y_coordinate = data[:,3]
-    pyCdraw.drawmoduleandazimuthdistribution(X_coordinate, Y_coordinate)
-    plt.show()
-    pyCdraw.drawdistribution(azimuths)
-    plt.show()
-    pyCdraw.drawhistogram(azimuths, classSize=15)
-    plt.show()
-    pyCdraw.drawPoints(X_coordinate, Y_coordinate, outlier_percent = 0.08)
-    plt.show()
-    pyCdraw.drawdensityMap(X_coordinate, Y_coordinate, bandwidth=10, paintpoint = True)
-    plt.show()
-    pyCdraw.drawqqplot(azimuths)
-    plt.show()
-    pyCdraw.drawVectors(data)
-    plt.show()
+        modules = data[:,0]
+        azimuths = data[:,1]
+        values = pyCmath.allmodulestatistics(data[:,0], ndig = 4)
+        print(values)
+        values = pyCmath.allazimuthstatistic(data[:,1], ndig = 4)
+        print(values)
+        values = pyCmath.raotest(azimuths)
+        print(values)
+        values = pyCmath.rayleightest(azimuths)
+        print(values)
+        X_coordinate = data[:,2]
+        Y_coordinate = data[:,3]
+        pyCdraw.drawmoduleandazimuthdistribution(X_coordinate, Y_coordinate)
+        plt.show()
+        pyCdraw.drawdistribution(azimuths)
+        plt.show()
+        pyCdraw.drawhistogram(azimuths, classSize=15)
+        plt.show()
+        pyCdraw.drawPoints(X_coordinate, Y_coordinate, outlier_percent = 0.08)
+        plt.show()
+        pyCdraw.drawdensityMap(X_coordinate, Y_coordinate, bandwidth=10, paintpoint = True)
+        plt.show()
+        pyCdraw.drawqqplot(azimuths)
+        plt.show()
 
 
 if __name__ == '__main__':
